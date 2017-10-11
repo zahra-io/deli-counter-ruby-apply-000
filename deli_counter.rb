c5ad5287c665d57868f8c5bf3b86bfb20d71bc7f
Zahra Khan <zarssskhan@gmail.com>
2017-10-11 08:47:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,10 +15,10 @@ end
 def take_a_number(katz_deli, name)
   # check if line is empty
   # add person to the line
-    if katz_deli.empty?
+    #if katz_deli.empty?
       katz_deli << name
-    elsif !katz_deli.empty?
-      katz_deli.push(name)
+  #  elsif !katz_deli.empty?
+    #  katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  end
+  #end
 end
