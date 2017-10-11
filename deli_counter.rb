3e63cef346534360049ae49accdf192dd310a976
Zahra Khan <zarssskhan@gmail.com>
2017-10-11 08:27:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_place = "The line is currently:"
-    
+    katz_deli.each.any? do |person, place|
+      current_place << " #{place}. #{person}"
 end
